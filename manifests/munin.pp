8915804d179ecaee671c03f42d7d6d1446310117
mh <mh@immerda.ch>
2009-02-21 18:15:23 +0000

    correcting source

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -2,23 +2,23 @@
 
 class pf::munin {
     munin::plugin::deploy{'pf': 
-        source => "puppet://$server/pf/munin/pf" ,
+        source => "pf/munin/pf" ,
         config => 'user root',
     }
     munin::plugin::deploy{'pf_bytes':
-        source => "puppet://$server/pf/munin/pf_bytes",
+        source => "pf/munin/pf_bytes",
         config => 'user root',
      }
     munin::plugin::deploy{'pf_packets':
-        source => "puppet://$server/pf/munin/pf_packets",
+        source => "pf/munin/pf_packets",
         config => 'user root',
     }
     munin::plugin::deploy{'pf_searches':
-        source => "puppet://$server/pf/munin/pf_searches",
+        source => "pf/munin/pf_searches",
         config => 'user root',
     }
     munin::plugin::deploy{'pf_states':
-        source => "puppet://$server/pf/munin/pf_states",
+        source => "pf/munin/pf_states",
         config => 'user root',
     }
 }
